81109e4355a17d74e72105fca443ed1002974ef9
swetha0 <gaali.swetha@gmail.com>
2020-05-26 21:56:12 -0400

    FSJ0420A104

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -8,6 +8,12 @@ public class Main {
 	public static void main(String args[]) {
 		
 	// Fill your code
-		
+		ConnectionManager manager= new ConnectionManager();
+		Connection con = manager.getConnection();
+		if(con!=null) {
+			System.out.println("Connection established");
+		}
+		else
+			System.out.println("Check your connection");
 	}
 }
